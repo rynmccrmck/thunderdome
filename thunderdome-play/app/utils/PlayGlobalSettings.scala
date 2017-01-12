33f2e0d515fb46175c6e0bab1ec2b062d72ab14e
rynmccrmck <ryanjmccormack@gmail.com>
2017-01-12 01:43:10 -0500

    silly 404

--- thunderdome-play/app/utils/PlayGlobalSettings.scala
+++ thunderdome-play/app/utils/PlayGlobalSettings.scala
@@ -7,7 +7,30 @@ import org.joda.time.DateTimeZone
 import play.api.{ Application, GlobalSettings }
 import services.database.Schema
 
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+
+import scala.concurrent.Future
+
 object PlayGlobalSettings extends GlobalSettings {
+
+/*    
+  // called when a route is found, but it was not possible to bind the request parameters
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    BadRequest("Bad Request: " + error)
+  } 
+
+  // 500 - internal server error
+  override def onError(request: RequestHeader, throwable: Throwable) = {
+    InternalServerError(views.html.notFoundPage(throwable))
+  }*/
+
+  // 404 - page not found error
+  override def onHandlerNotFound(request: RequestHeader): scala.concurrent.Future[play.api.mvc.Result] = {
+    Future.successful(NotFound(views.html.notFoundPage(request)))
+  }
+  
   override def onStart(app: Application) = {
     DateTimeZone.setDefault(DateTimeZone.UTC)
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
