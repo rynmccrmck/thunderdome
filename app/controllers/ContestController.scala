50400a68e6787cab2a4758b862e6e71386f4fa45
rynmccrmck <ryanjmccormack@gmail.com>
2017-01-21 18:38:34 -0500

    working test set submission finally

--- app/controllers/ContestController.scala
+++ app/controllers/ContestController.scala
@@ -4,6 +4,7 @@ import com.mohiva.play.silhouette.api.{ LoginEvent, LoginInfo, SignUpEvent }
 import com.mohiva.play.silhouette.impl.providers.{ CommonSocialProfile, CredentialsProvider }
 import models.user.{ RegistrationData, UserForms }
 
+import play.api.libs.Files.TemporaryFile
 import play.api.i18n.{ Messages, MessagesApi }
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.AnyContent
@@ -14,7 +15,9 @@ import models.user._
 import models.queries.ProfileQueries
 import scala.concurrent.Future
 import services.admin._
-
+import play.api._
+import play.api.mvc._
+import com.mohiva.play.silhouette.api._
 @javax.inject.Singleton
 class ContestController @javax.inject.Inject() (
     override val messagesApi: MessagesApi,
@@ -39,12 +42,29 @@ class ContestController @javax.inject.Inject() (
     }
   }
   
-  def create = withSession { implicit request =>
-    val test = UserForms.contestForm.bindFromRequest()
-    println(test)
-    test.fold(
-      formWithErrors =>  Future.successful(BadRequest(views.html.index(request.identity))),
-      contest =>  {ContestCreateService.save(request.identity,contest); Future.successful(Ok(s"Contest ${contest.contest_name} created successfully"))}
+  def uploadTestSet(request: Request[MultipartFormData[TemporaryFile]],contest_id:String): String = {
+    Logger.error("Called uploadFile function" + request)
+    request.body.file("testset").map { picture =>
+      import java.io.File
+      val filename = picture.filename
+      val contentType = picture.contentType
+      Logger.error(s"File name : $filename, content type : $contentType")
+      picture.ref.moveTo(new File(s"/tmp/${contest_id}_testset"))
+      "File uploaded"
+    }.getOrElse {
+      "Missing file"
+    }
+  }  
+
+  def create = UserAwareAction.async(parse.multipartFormData)  { implicit request =>
+    val contestSubmission = UserForms.contestForm.bindFromRequest()
+    contestSubmission.fold(
+      formWithErrors =>  Future.successful(BadRequest("ARG")),
+      contest =>  {
+         ContestCreateService.save(request.identity.get,contest);
+         uploadTestSet(request,contest.contest_name)
+         Future.successful(Ok(s"Contest ${contest.contest_name} created successfully"))
+      }
     )
   }
 
